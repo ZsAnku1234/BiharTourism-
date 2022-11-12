1281e71d900de8d577b0147e5fbe323cae6534f7
Anku <akanku1234kr@gmail.com>
2022-11-12 15:16:37 +0530

    Initial Commit

---
+++ JS/01.js
@@ -0,0 +1,21 @@
+function advanture() {
+    window.open("http://127.0.0.1:5501/advanture.html")
+}
+function culture() {
+    window.open("http://127.0.0.1:5501/culture.html")
+}
+function food() {
+    window.open("http://127.0.0.1:5501/food.html")
+}
+function temple() {
+    window.open("https://en.wikipedia.org/wiki/List_of_Hindu_temples_in_Bihar")
+}
+function nature() {
+    window.open("https://in.pinterest.com/pin/594123375836220028/")
+}
+function wildlife() {
+    window.open("https://www.thrillophilia.com/wildlife-in-bihar#:~:text=Wildlife%20Found%3A%20Animals%20like%20Leopard,wild%20bears%20in%20this%20area.")
+}
+function history() {
+    window.open("https://en.wikipedia.org/wiki/History_of_Bihar")
+}
